12a42f19c88d594f2e7d172e5a55561951cf9f77
fed <platinumaccount@yandex.ru>
2019-02-09 02:45:13 +0300

    handlers install data

---
+++ -/data/install/handlers.php
@@ -0,0 +1,8 @@
+<?php return [
+    'import_method_path' => '\ewma\handlers app/exchange:importCat',
+    'target_cat_path'    => 'modules/ewma-handlers',
+    'skip_first_level'   => true,
+    'input_files'        => [
+        'main'
+    ]
+];
